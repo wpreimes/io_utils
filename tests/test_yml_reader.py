9e76d49bef5acdf110c33a09cc5368ed0d91d1f1
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2021-06-11 14:42:45 +0200

    Develop, not install

--- tests/test_yml_reader.py
+++ tests/test_yml_reader.py
@@ -4,9 +4,9 @@ import os
 from io_utils.yml.read import spaceify, read_settings
 import numpy as np
 import sys
-import io_utils.root_path as root_path
 
-yml_path = os.path.join(root_path.test_root, '00_testdata', 'yml', 'test_config.yml')
+this_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+yml_path = os.path.join(this_path, '00_testdata', 'yml', 'test_config.yml')
 
 def test_read_yml():
     SPACES = spaceify(yml_path)
